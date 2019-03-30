924b31822351fea6008296fc86ce06759d937def
zavh <hossain.zaved@gmail.com>
2019-03-30 19:00:32 +0600

    Statistic table creation

---
+++ database/migrations/2019_03_30_125417_create_statistics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('record_time');
+            $table->float('voltage', 6,2);
+            $table->float('current', 6,2);
+            $table->float('power', 6,2);
+            $table->float('temperature', 6,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
