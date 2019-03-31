d7aee3d8acc8da6ca7a4a022ce4860f24e5e0bce
zavh <hossain.zaved@gmail.com>
2019-03-31 14:14:45 +0600

    SSE Initial  working implementation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,10 +28,10 @@ class UserController extends Controller
     }
     public function login(Request $request)
     {
-        $user = \App\User::where('email', $request->email)->get()->first();
+        $user = \App\User::where('email', strtolower($request->email))->get()->first();
         if ($user && \Hash::check($request->password, $user->password)) // The passwords match...
         {
-            $token = self::getToken($request->email, $request->password);
+            $token = self::getToken(strtolower($request->email), $request->password);
             $user->auth_token = $token;
             $user->save();
             $response = ['success'=>true, 'data'=>['id'=>$user->id,'auth_token'=>$user->auth_token,'name'=>$user->name, 'email'=>$user->email]];
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use Symfony\Component\HttpFoundation\StreamedResponse;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,9 +26,29 @@ Route::middleware(['cors'])->group(function () {
 
 Route::middleware(['jwt.auth','cors'])->group(function () {
 
-    Route::get('users/list', function(){
+    Route::get('/users/list', function(){
         $users = App\User::all();
         $response = ['success'=>true, 'data'=>$users];
         return response()->json($response, 201);
+    });
+
+    Route::get('/sse', function(){
+
+        $response = new StreamedResponse();
+        $response->setCallback(function () {
+            $serverTime = time();        
+            $data['id'] = $serverTime;
+            $data['data'] = date("h:i:s", time());
+            echo 'data: ' . json_encode($data) .PHP_EOL;
+            echo 'retry :10000'.PHP_EOL;
+            ob_flush();
+            flush();
+            sleep(60);
+        });
+        $response->headers->set('Content-Type', 'text/event-stream');
+        $response->headers->set('X-Accel-Buffering', 'no');
+        $response->headers->set('Cache-Control', 'no-cache');
+        
+        return $response;
     });
 });
